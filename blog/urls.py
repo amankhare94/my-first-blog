48653c0192d7a2663b402f0bb915c316e4cf5a5b
amankhare <amankhare94@gmail.com>
2018-11-26 10:24:14 +0530

    adding forms

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    url('post/list/', views.post_list, name='post_list'),
+    url('post/new/', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url('', views.post_list, name='post_list'),
 ]
+
+print urlpatterns
